c6ac47fbce5a595f0fae7f453d012888b86658e5
jkmbo2 <2432616197@qq.com>
2026-05-20 20:45:01 +0800

    终于把历史记录做完了，好累，后面复习一下管道，历史记录虽然代码量大，但是管道概念复杂

--- minishell.cpp
+++ minishell.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <sstream>
 #include <vector>
-#include <unistd.h> // STDFILENO标准输入的fd
+#include <unistd.h> // STDFILENO标准输入的fd, read()
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <climits>
@@ -16,6 +16,8 @@ int his_index = 0;
 
 struct termios orig_termios;
 
+void printPromote();
+
 void enableRawMode()
 {
 	tcgetattr(STDIN_FILENO, &orig_termios);
@@ -24,14 +26,15 @@ void enableRawMode()
 	// 为什么要用位处理是因为c_lflag一般是一个32位或者64位的整数，而像ECHO和ICANON这种开关就是其中一位，
 	// 也就是说对于ECHO和ICANON这种开关，就是一个所有位数上只有特定位为1的整数
 	// 关回显是因为如果不关的话，输入的非正常字符如退格键，空格键等等都会显示出来，所以要关掉，然后自己来控制屏幕输出
-	raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG);// ISIG是input signal的意思，关了之后终端对Ctrl+C这种特殊输入就是当成字符处理了，而不是中断程序
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);// 中间的参数指的是什么时候应用新设置
 }
 
 void disableRawMode()
 {
-	
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
+
 
 // 拆分用户的输入并执行命令
 void divideAndExecute(const vector<string>& args)
@@ -160,19 +163,107 @@ void execute(const vector<string>& args)
 	}
 }
 
+
+void printPromote()
+{
+	char address[PATH_MAX];
+
+	cout << "sofei@" << getcwd(address, sizeof address);
+	cout << ">>";
+}
+
+string read_line_with_history()
+{
+	cout << unitbuf;
+	enableRawMode();
+
+	string buffer;
+	string saved_line;// 用户点上箭头时缓存当前行，因为当前行没被存进history
+
+	while (true)
+	{
+		char ch;
+		read(STDIN_FILENO, &ch, 1);
+
+		if (ch == '\n' || ch == '\r')// r是回车
+		{
+			cout << endl;
+			disableRawMode();
+			history.push_back(buffer);
+			his_index = history.size();
+			return buffer;
+		}
+		else if (ch == 127)// 退格键
+		{
+			if (!buffer.empty())
+			{
+				buffer.pop_back();// 删除最后一个字符
+				// 先左移光标然后用空格覆盖原来的字符，但是这时打印空格后光标又会右移，所以还需要左移一次
+				std::cout << "\033[" << 1 << "D";
+				cout << " ";
+				std::cout << "\033[" << 1 << "D";
+			}
+		}
+		else if (ch == 3)// Ctrl+C
+		{
+			cout << "^C\n";
+			disableRawMode();// 不关的话终端会卡死，因为回显没开的话用户看不到输入的内容
+			return " ";
+		}
+		else if (ch == '\033')// 033八进制，实际上是27，Esc键
+		{
+			// \033[A就是上箭头，A改成B就是下箭头
+			read(STDIN_FILENO, &ch, 1);
+			read(STDIN_FILENO, &ch, 1);
+			if (ch == 'A' && his_index > 0 && !history.empty())
+			{
+				cout << "\033[2K\r";// \033[2K清除整行，但是不移动光标，\r将光标移到行首
+				printPromote();
+				if (his_index == history.size())
+					saved_line = buffer;// 只存第一次按上箭头前的那一行
+				buffer = history[his_index - 1];
+				cout << buffer;
+				his_index--;
+			}
+			else if (ch == 'B')
+			{
+				if (his_index + 1 >= history.size())
+				{
+					cout << "\033[2K\r";
+					buffer = saved_line;
+					printPromote();
+					cout << buffer;
+					his_index = history.size();
+				}
+				else
+				{
+					cout << "\033[2K\r";
+					buffer = history[his_index + 1];
+					printPromote();
+					cout << buffer;
+					his_index++;
+				}
+			}
+		}
+		else
+		{
+			buffer.push_back(ch);
+			cout << ch;
+		}
+	}
+}
+
 void read_user_input()
 {
 	string input;
-	char buffer[PATH_MAX];
-
-	cout << "sofei@" << getcwd(buffer, sizeof buffer);
-	cout << ">>";
-	while (getline(cin, input))
+	printPromote();
+	cout << flush;// 虽然在read_line_with_history中设置了unitbuf，但是这个函数在下面才调用
+	while (true)
 	{
-		history.push_back(input);
+		input = read_line_with_history();
 		execute(eval(input));
-		cout << "\nsofei@" << getcwd(buffer, sizeof buffer);
-		cout << ">>";
+	
+		printPromote();
 	}
 }
 
